73c9b96e997b801c94a5f4d528e91cdd2e7d9d1b
olegmlsn <4olegmlsn@gmail.com>
2023-02-20 00:26:05 +0500

    Initialization draft

--- chabot.go
+++ chabot.go
@@ -1,7 +1,22 @@
 package chabot
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+)
 
-func Bla(s string) {
-	fmt.Println(s)
+type Chabot struct {
+	BotApiKey   string
+	ChannelName string
+}
+
+func New() *Chabot {
+	return &Chabot{
+		BotApiKey:   os.Getenv("BOT_API_KEY"),
+		ChannelName: os.Getenv("CHANNEL_NAME"),
+	}
+}
+
+func (c Chabot) Bla() {
+	fmt.Println(c.BotApiKey, c.ChannelName)
 }
